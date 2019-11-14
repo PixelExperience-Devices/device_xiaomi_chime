41b73c0829a276c901810436adb2f1a46aaa8a94
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-13 21:45:50 -0600

    chime: Enable config_vibrateOnIconAnimation * Need those sexy vibes.
    Change-Id: If5671135879f6f4069d0ed881b41b0a243890349
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- device.mk
+++ device.mk
@@ -303,6 +303,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
+    FrameworksCustomChime \
     FrameworksResChime \
     SettingsResChime \
     SystemUIResChime \
