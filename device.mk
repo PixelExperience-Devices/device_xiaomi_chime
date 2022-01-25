ae5dc31e5bf44fff06f96c9f019d7094ef56d697
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    chime: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
