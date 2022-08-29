7ffa84b57ebf922ce27fd4d7be850634eb6baaec
HeroBuxx <herobuxx@gmail.com>
2022-08-29 14:52:19 +0000

    chime: Build AOSP Recovery
    - User are not recommended to use custom recoverries such as TWRP and forks
    - Provide AOSP Recovery to flash builds
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- aosp_chime.mk
+++ aosp_chime.mk
@@ -13,6 +13,7 @@ $(call inherit-product, device/xiaomi/chime/device.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
