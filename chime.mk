86899413eb062b8ec9d2cd92d216c3918b954287
HeroBuxx <me@buxxed.tk>
2023-01-28 20:18:42 +0000

    chime: Build vendor.display.config@1.9
    01-29 03:00:37.945   495   495 I hwservicemanager: getTransport: Cannot find entry vendor.display.config@1.0::IDisplayConfig/default in either framework or device VINTF manifest.
    
    Causes camera to stop working
    
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- chime.mk
+++ chime.mk
@@ -113,8 +113,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.14 \
+    vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
