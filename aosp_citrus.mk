58075e6690b72ec018c915c748f752521cd5ed64
HeroBuxx <me@buxxed.tk>
2023-01-28 18:05:08 +0000

    chime: Now let's do PixelExperience
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- aosp_citrus.mk
+++ aosp_citrus.mk
@@ -7,11 +7,14 @@
 # Inherit from citrus device
 $(call inherit-product, device/xiaomi/chime/device-citrus.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_citrus
+PRODUCT_NAME := aosp_citrus
 PRODUCT_DEVICE := citrus
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO M3
--- aosp_lime.mk
+++ aosp_lime.mk
@@ -7,11 +7,14 @@
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/chime/device-lime.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lime
+PRODUCT_NAME := aosp_lime
 PRODUCT_DEVICE := lime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 9T
