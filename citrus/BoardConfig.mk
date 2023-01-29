9e8e6f41e00f8151154aead59786b6b816744c07
HeroBuxx <me@buxxed.tk>
2023-01-29 11:52:15 +0000

    chime: Include prebuilt DTBO Image of each devices
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- citrus/BoardConfig.mk
+++ citrus/BoardConfig.mk
@@ -12,5 +12,11 @@ include device/xiaomi/chime/BoardConfig-chime.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := citrus,chime,juice
 
+# Kernel - Prebuilt images
+#BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/images/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/images/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/citrus/BoardConfigVendor.mk
--- lime/BoardConfig.mk
+++ lime/BoardConfig.mk
@@ -12,6 +12,12 @@ include device/xiaomi/chime/BoardConfig-chime.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := lime,lemon,pomelo,chime,juice
 
+# Kernel - Prebuilt images
+#BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/images/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/images/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # NFC
 ODM_MANIFEST_SKUS += lemon
 ODM_MANIFEST_LEMON_FILES := $(DEVICE_PATH)/vintf/manifest_lemon.xml
