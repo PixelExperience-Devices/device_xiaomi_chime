672230995bbfce1cebd394ffd1728ee0b02e7ee8
HeroBuxx <me@herobuxx.me>
2023-03-15 13:46:05 +0000

    chime: Copy Touch firmware for recovery image
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- extract-files.sh
+++ extract-files.sh
@@ -56,6 +56,7 @@ fi
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
+extract "${MY_DIR}/proprietary-files-recovery.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
