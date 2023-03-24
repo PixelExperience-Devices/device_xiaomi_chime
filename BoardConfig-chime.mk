f29f2f3f78967d241a08d6c930720ff3cdbba2ff
HeroBuxx <me@herobuxx.me>
2023-03-24 17:08:05 +0000

    chime: Switch to fragment kernel config
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- BoardConfig-chime.mk
+++ BoardConfig-chime.mk
@@ -54,7 +54,7 @@ BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/chime
-TARGET_KERNEL_CONFIG := vendor/chime_defconfig
+TARGET_KERNEL_CONFIG := vendor/xiaomi/bengal_defconfig vendor/xiaomi/chime.config
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 #TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
