d0678b6c61861bf65f47a8e99ab01b73c756aaa6
Rockford the Roe <egstein01@gmail.com>
2023-05-10 00:22:05 -0500

    Revert "chime: Include prebuilt DTBO Image of each devices"
    This reverts commit 9e8e6f41e00f8151154aead59786b6b816744c07.

--- citrus/BoardConfig.mk
+++ citrus/BoardConfig.mk
@@ -12,11 +12,5 @@ include device/xiaomi/sm6115-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := citrus,chime,juice
 
-# Kernel - Prebuilt images
-#BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/images/dtbo.img
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/images/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-
 # Inherit from the proprietary version
 include vendor/xiaomi/citrus/BoardConfigVendor.mk
