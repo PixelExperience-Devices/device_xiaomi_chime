8a40a5d1201261ed5c5634660fb25216bf3db796
rockfordroeNG <130606096+rockfordroeNG@users.noreply.github.com>
2023-05-10 07:13:47 +0000

    Update chime.mk

--- chime.mk
+++ chime.mk
@@ -305,7 +305,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlays
+    $(LOCAL_PATH)/overlays \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
