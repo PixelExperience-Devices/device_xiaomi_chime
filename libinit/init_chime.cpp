b30eda25cb37cdfeb98e40c8c402ae41a9bd9a30
HeroBuxx <me@herobuxx.me>
2023-09-17 18:38:10 +0000

    chime: libinit: Apply lime build fingerprint for lemon and pomelo
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- libinit/init_chime.cpp
+++ libinit/init_chime.cpp
@@ -30,7 +30,7 @@ static const variant_info_t lime_info = {
     .device = "lime",
     .marketname = "Redmi 9T",
     .model = "M2010J19SG",
-    .build_fingerprint = "Redmi/lime/lime:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lime_global/lime:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
 
     .nfc = false,
 };
@@ -43,7 +43,7 @@ static const variant_info_t pomelo_info = {
     .device = "pomelo",
     .marketname = "Redmi 9 Power",
     .model = "M2010J19SL",
-    .build_fingerprint = "Redmi/pomelo/pomelo:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lime_global/lime:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
 
     .nfc = false,
 };
@@ -56,7 +56,7 @@ static const variant_info_t lemon_info = {
     .device = "lemon",
     .marketname = "Redmi 9T NFC",
     .model = "M2010J19SY",
-    .build_fingerprint = "Redmi/lemon/lemon:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lime_global/lime:12/RKQ1.211130.001/V14.0.5.0.SJQMIXM:user/release-keys",
 
     .nfc = true,
 };
